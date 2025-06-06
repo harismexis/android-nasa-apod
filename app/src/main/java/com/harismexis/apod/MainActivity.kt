c29703c282ce14a12fc5d0e2ab9aaecca0369aee
Haris Mexis <theoharismexis@hotmail.com>
2025-06-06 06:09:02 +0100

    Remove AppViewModelFactory

--- app/src/main/java/com/harismexis/apod/MainActivity.kt
+++ app/src/main/java/com/harismexis/apod/MainActivity.kt
@@ -8,7 +8,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
-import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -24,14 +23,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import com.harismexis.apod.route.ApodRoute
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.harismexis.apod.dialog.DatePickerModal
-import com.harismexis.apod.model.isImage
-import com.harismexis.apod.model.isVideo
+import com.harismexis.apod.route.ApodRoute
 import com.harismexis.apod.ui.theme.NasaApisAppTheme
 import com.harismexis.apod.viewmodel.ApodViewModel
-import com.harismexis.apod.viewmodel.createViewModel
 
 class MainActivity : ComponentActivity() {
 
@@ -41,7 +37,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NasaApisAppTheme {
-                val viewModel: ApodViewModel = createViewModel()
+                val viewModel: ApodViewModel = viewModel()
                 Scaffold(
                     topBar = { SmallTopAppBar(viewModel) },
                 ) { padding ->
